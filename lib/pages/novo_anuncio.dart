dd1b2660115b46d2b13b0bb0d1751fdcaff48c11
enzomp <enzopetry@hotmail.com>
2020-06-16 23:54:32 -0300

    Início do novo anuncio

--- lib/pages/novo_anuncio.dart
+++ lib/pages/novo_anuncio.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class NovoAnuncio extends StatelessWidget{
+class NovoAnuncio extends StatefulWidget {
+  @override
+  _NovoAnuncioState createState() => _NovoAnuncioState();
+}
+
+class _NovoAnuncioState extends State<NovoAnuncio> {
+
+  File _foto;
+
+  Future _getImage() async {
+    PickedFile img;
+    ImagePicker cam = ImagePicker();
+    img = await cam.getImage(source: ImageSource.camera);
+
+    setState(() {
+      _foto = File(img.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,6 +32,10 @@ class NovoAnuncio extends StatelessWidget{
           ),
           Align(
             alignment: Alignment.bottomCenter,
+            child: _title(),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
             child: _content(),
           ),
           Align(
@@ -20,7 +44,15 @@ class NovoAnuncio extends StatelessWidget{
           ),
           Align(
             alignment: Alignment.topLeft,
+            child: _addPhoto(context),
+          ),
+          Align(
+            alignment: Alignment.topLeft,
             child: _backBtn(context),
+          ),
+          Align(
+            alignment: Alignment.topLeft,
+            child: _avatar,
           ),
         ],
       ),
@@ -34,6 +66,29 @@ class NovoAnuncio extends StatelessWidget{
     );
   }
 
+  Widget _title(){
+    return Container(
+      height: 450,
+      width: 510,
+      decoration: BoxDecoration(
+        color: Colors.amber,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+      ),
+      child: Text(
+          "Criar Anúncio",
+          textAlign: TextAlign.center,
+
+          style: TextStyle(
+            fontFamily: 'Oswald',
+            fontSize: 25,
+            color: Colors.white),
+          )
+    );
+  }
+
   Widget _action(){
     return Container(
       width: 200,
@@ -41,13 +96,13 @@ class NovoAnuncio extends StatelessWidget{
       margin: EdgeInsets.only(bottom: 45),
       child: FlatButton(
         shape: RoundedRectangleBorder(
-          borderRadius: new BorderRadius.circular(15.0),
-          side: BorderSide(color: Colors.amber)
+            borderRadius: new BorderRadius.circular(15.0),
+            side: BorderSide(color: Colors.amber)
         ),
         child: Text('ADICIONAR',
           style: TextStyle(
-            fontFamily: 'Oswald',
-            color: Colors.white
+              fontFamily: 'Oswald',
+              color: Colors.white
           ),
         ),
         color: Colors.amber,
